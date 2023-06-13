17d59751ea4925aa37d31692e75152e535146540
Kevin Park <kevin@fruitful.gg>
2023-06-13 23:29:30 +0800

    init a pool with PoolManager

--- test/Uv4.t.sol
+++ test/Uv4.t.sol
@@ -5,12 +5,108 @@ import { PRBTest } from "@prb/test/PRBTest.sol";
 import { console2 } from "forge-std/console2.sol";
 import { StdCheats } from "forge-std/StdCheats.sol";
 
+import { Hooks as HooksLib } from "v4-core/libraries/Hooks.sol";
+import { Currency } from "v4-core/libraries/CurrencyLibrary.sol";
 import { FixedPoint96 } from "v4-core/libraries/FixedPoint96.sol";
-import { IHooks } from "v4-core/interfaces/IHooks.sol";
+import { TickMath } from "v4-core/libraries/TickMath.sol";
+import { IHooks, BalanceDelta } from "v4-core/interfaces/IHooks.sol";
 import { IPoolManager } from "v4-core/interfaces/IPoolManager.sol";
 import { PoolManager } from "v4-core/PoolManager.sol";
 
-contract Hooks is IHooks { }
+contract Hooks is IHooks {
+    function beforeInitialize(
+        address sender,
+        IPoolManager.PoolKey calldata key,
+        uint160 sqrtPriceX96
+    )
+        external
+        returns (bytes4)
+    {
+        return this.beforeInitialize.selector;
+    }
+
+    function afterInitialize(
+        address sender,
+        IPoolManager.PoolKey calldata key,
+        uint160 sqrtPriceX96,
+        int24 tick
+    )
+        external
+        returns (bytes4)
+    {
+        return this.afterInitialize.selector;
+    }
+
+    function beforeModifyPosition(
+        address sender,
+        IPoolManager.PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata params
+    )
+        external
+        returns (bytes4)
+    {
+        return this.beforeModifyPosition.selector;
+    }
+
+    function afterModifyPosition(
+        address sender,
+        IPoolManager.PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata params,
+        BalanceDelta delta
+    )
+        external
+        returns (bytes4)
+    {
+        return this.afterModifyPosition.selector;
+    }
+
+    function beforeSwap(
+        address sender,
+        IPoolManager.PoolKey calldata key,
+        IPoolManager.SwapParams calldata params
+    )
+        external
+        returns (bytes4)
+    {
+        return this.beforeSwap.selector;
+    }
+
+    function afterSwap(
+        address sender,
+        IPoolManager.PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        BalanceDelta delta
+    )
+        external
+        returns (bytes4)
+    {
+        return this.afterSwap.selector;
+    }
+
+    function beforeDonate(
+        address sender,
+        IPoolManager.PoolKey calldata key,
+        uint256 amount0,
+        uint256 amount1
+    )
+        external
+        returns (bytes4)
+    {
+        return this.beforeDonate.selector;
+    }
+
+    function afterDonate(
+        address sender,
+        IPoolManager.PoolKey calldata key,
+        uint256 amount0,
+        uint256 amount1
+    )
+        external
+        returns (bytes4)
+    {
+        return this.afterDonate.selector;
+    }
+}
 
 /// @dev If this is your first time with Forge, read this tutorial in the Foundry Book:
 /// https://book.getfoundry.sh/forge/writing-tests
@@ -18,14 +114,14 @@ contract Uv4Test is PRBTest, StdCheats {
     PoolManager internal poolManager;
     Hooks internal hooks;
 
-    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address internal constant UNI = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
+    Currency internal constant WETH = Currency.wrap(address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
+    Currency internal constant UNI = Currency.wrap(address(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984));
     /// @dev A function invoked before each test case is run.
 
     function setUp() public virtual {
         // Instantiate the contract-under-test.
         poolManager = new PoolManager(1 ether);
-        vm.etch(0x9000000000000000000000000000000000000000, Hooks.code);
+        vm.etch(0x9000000000000000000000000000000000000000, type(Hooks).runtimeCode);
     }
 
     /// @dev Basic test. Run it with `forge test -vvv` to see the console log.
@@ -40,36 +136,9 @@ contract Uv4Test is PRBTest, StdCheats {
             /// @notice Ticks that involve positions must be a multiple of tick spacing
             tickSpacing: 60,
             /// @notice The hooks of the pool
-            hooks: 0x9000000000000000000000000000000000000000
+            hooks: hooks
         });
 
-        poolManager.initialize(poolKey, FixedPoint96.Q96);
+        poolManager.initialize(poolKey, TickMath.getSqrtRatioAtTick(0));
     }
-
-    // /// @dev Fuzz test that provides random values for an unsigned integer, but which rejects zero as an input.
-    // /// If you need more sophisticated input validation, you should use the `bound` utility instead.
-    // /// See https://twitter.com/PaulRBerg/status/1622558791685242880
-    // function testFuzz_Example(uint256 x) external {
-    //     vm.assume(x != 0); // or x = bound(x, 1, 100)
-    //     assertEq(foo.id(x), x, "value mismatch");
-    // }
-
-    // /// @dev Fork test that runs against an Ethereum Mainnet fork. For this to work, you need to set
-    // `API_KEY_ALCHEMY`
-    // /// in your environment You can get an API key for free at https://alchemy.com.
-    // function testFork_Example() external {
-    //     // Silently pass this test if there is no API key.
-    //     string memory alchemyApiKey = vm.envOr("API_KEY_ALCHEMY", string(""));
-    //     if (bytes(alchemyApiKey).length == 0) {
-    //         return;
-    //     }
-
-    //     // Otherwise, run the test against the mainnet fork.
-    //     vm.createSelectFork({ urlOrAlias: "mainnet", blockNumber: 16_428_000 });
-    //     address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    //     address holder = 0x7713974908Be4BEd47172370115e8b1219F4A5f0;
-    //     uint256 actualBalance = IERC20(usdc).balanceOf(holder);
-    //     uint256 expectedBalance = 196_307_713.810457e6;
-    //     assertEq(actualBalance, expectedBalance);
-    // }
 }
